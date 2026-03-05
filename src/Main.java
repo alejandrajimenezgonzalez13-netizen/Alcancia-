3c782c828b6f9a54306469b64bace3ab04672e76
Maria Alejandra <tucorreo@gmail.com>
2026-03-05 13:20:16 -0500

    Trabajo Alcancia

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /* Desarrollar un algoritmo que pueda simular, en dicha alcancia se pueda guardar monedas
+        de las siguiente 20, 50 ,1000 , no se pueden guarda billetes ni monedas de otras
+        descripcione, al dueño de la alcancia le parece util conocer cuanto dinero tiene
+        ahorrado sin romperla , es ma el quiere saber cuantas monedas tiene de cada denominacion
+        y cual es el monto acumulado por cada una de elllas.
+        el sistema debe contar con un menu de interacion
+        1. agregar monedas de cada uno de las denominaciones
+        2. contar cuantas monedas tiene de cada denominacio.
+        3. indagar cual es el valor acumulado por cada una de las denominaciones.
+        4. calcular el total del dinero ahorrado.
+        5. establecer una meta de ahorro mostrando el progreso.
+         */
+
+
+
+
+        }
+    }
+}
